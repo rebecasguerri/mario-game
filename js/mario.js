a091736d2e636043f769110f00ba9c1e75e2eace
rebecafreitass <rebecasguerri@gmail.com>
2023-04-25 18:25:48 -0300

    update

--- js/mario.js
+++ js/mario.js
@@ -1,8 +1,15 @@
-const mario =document.querySelector("#mario");
-const pipe =document.querySelector("#pipe");
+const mario =document.querySelector("#mario")
+const pipe =document.querySelector("#pipe")
+const marioJS = window.matchMedia("(max-width: 430px)");
+const marioDeath = document.querySelector("#mario_death")
+const audioJump = document.querySelector("#audio-jump")
+const textStart = document.querySelector('#text-start')
 
 const jump = () =>{
     mario.classList.add('jump')
+    audioJump.currentTime = 0.1;
+    audioJump.volume = 0.1;
+    audioJump.play();
     setTimeout(() => {
         mario.classList.remove('jump')
     },500)
@@ -13,10 +20,20 @@ const loop = setInterval(() =>{
     console.log(marioPosition)
     if(pipePosition  <= 143 & pipePosition >0 & marioPosition < 60){
         pipe.style.animation= "none"
-        pipe.style.left = '15%';
+        pipe.style.left = `${pipePosition}px`;
         mario.style.animation= "none"
-        mario.style.left = '15%';
+        mario.style.bottom = `${marioPosition}px`;
+        mario.src ="./images/game-over.png";
+        mario.style.width = "6rem";
+        mario.style.left="3rem";
+        marioDeath.currentTime = 0.1;
+        marioDeath.volume = 0.2;
+        marioDeath.play();
+        document.getElementById("text-start").style.color = "black";
+        document.getElementById("text-start").innerHTML = "<strong>GAME OVER</strong>";
+        clearInterval(loop)
     }
 }, 10)
+
 document.addEventListener('click', jump)
 document.addEventListener('keydown', jump)
